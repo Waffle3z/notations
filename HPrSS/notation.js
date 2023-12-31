06b5b7f12c47ce4f4d6d0042a81f374f32927b5d
Waffle <w4ffle3z@gmail.com>
2023-12-30 22:07:41 -0500

    fix HPrSS

--- HPrSS/notation.js
+++ HPrSS/notation.js
@@ -12,31 +12,40 @@ class notation {
 	}
 
 	static expand(a, n) {
-		function p(n) {
-			if (n === 0) return 0;
+		function parent(n) {
+			if (a[n] === 0) return 0;
 			let r = n;
 			while (a[r] >= a[n]) r--;
 			return r;
 		}
-		let r = p(a.length-1);
-		if (a[r] == a[a.length-1]-1) return [...a.slice(0, r), ...Array(n).fill(a.slice(r, -1)).flat()];
 
-		let t = a.map((v, i) => v - a[p(i)]); // differential sequence
+		let root = parent(a.length-1);
+		if (a[root] == a[a.length-1]-1) {
+			let out = a.slice(0, root);
+			let badPart = a.slice(root, -1);
+			for (let i = 0; i < n; i++) {
+				out = out.concat(badPart);
+			}
+			return out;
+		}
+
+		let differences = a.map((v, i) => v - a[parent(i)]);
+
 		let q = [];
 		for (let i = 0; i < a.length; i++) {
-			if (t[i] < t[a.length-1]) q.push(i);
+			if (differences[i] < differences[a.length-1]) q.push(i);
 		}
-		let k = [a.length-1];
-		while (k[k.length-1] != 0) k.push(p(k[k.length-1]));
-		r = Math.max(...q.filter((v) => k.includes(v)));
+		root = a.length-1;
+		while (root != 0 && !q.includes(root)) root = parent(root);
 
-		let d = a[a.length-1] - a[r] - 1;
-		let o = a.slice(0, r);
+		let diff = a[a.length-1] - a[root] - 1;
+		let out = a.slice(0, root);
+		let badPart = a.slice(root, -1);
 		for (let i = 0; i < n; i++) {
-			o = o.concat(a.slice(r, -1).map((j) => j + d * i));
+			out = out.concat(badPart.map((j) => j + diff * i));
 		}
 
-		return o;
+		return out;
 	}
 
 	static isSuccessor(array) {
