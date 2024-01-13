3516e9a7a06797095279ed2bdb3784555a333eba
Waffle <w4ffle3z@gmail.com>
2024-01-13 15:06:50 -0500

    fix eulerian sequence expansion

--- Eulerian/notation.js
+++ Eulerian/notation.js
@@ -63,18 +63,18 @@ function calcMountain(row) {
     			}
 				newRow.push({
 					value: row[i].delta,
-					position: row[i].position - 1,
+					position: row[i].position,
 					parentIndex: -1,
 					delta: 0
 				});
 			}
 		}
 		for (let i = 0; i < newRow.length; i++) {
-			let p = row.findIndex(x => x.position > newRow[i].position);
+			let p = row.findIndex(x => x.position >= newRow[i].position);
 			while (p >= 0) {
 				p = row[p].parentIndex;
 				if (p < 0) break;
-				let j = newRow.findIndex(x => x.position >= row[p].position - 1);
+				let j = newRow.findIndex(x => x.position >= row[p].position);
 				if (j < 0 || (j < newRow.length-1 && newRow[j].position + 1 != newRow[j+1].position)) break;
 				if (newRow[j].value < newRow[i].value) {
 					newRow[i].parentIndex = j;
@@ -112,130 +112,102 @@ function cloneMountain(mountain) {
 	}));
 }
 
-function getBadRoot(mountain) {
-	let i = mountain.findLastIndex((v, i) => v.at(-1).position + i == mountain[0].length-1);
-	return mountain[i-1][mountain[i-1].at(-1).parentIndex].position + i-1;
-}
-
-function TrSSexpand(a, n) {
-	function parent(ind) {
-		return a.findLastIndex((v, i) => (i < ind || i == 0) && (v == 0 || v < a[ind]));
-	}
-	let cutNode = a.at(-1);
-	let root = a.length - 1;
-	let diff = cutNode - a[parent(a.length - 1)];
-	let delta = 0;
-	for (let i = 0; i < diff; i++) {
-		if (a[root] == 0) {
-			delta++;
-		} else {
-			root = parent(root);
-		}
-	}
-	let badPart = a.slice(root, -1);
-	let increment = cutNode - a[root] - 1;
-	a.pop();
-	if (delta == 0) {
-		for (let i = 1; i <= n; i++) {
-			a.push(...badPart.map(v => v + increment*i));
-		}
-	} else {
-		let badPartHeight = 1;
-		let bottom = badPart.length - 1;
-		while (bottom > 0) {
-			bottom = badPart.findLastIndex((v, i) => i < bottom && v < badPart[bottom]);
-			badPartHeight++;
-		}
-
-		for (let i = 1; i <= n; i++) {
-			a.push(...badPart.map(v => v + increment));
-			increment += badPart.at(-1) + badPartHeight * i + delta;
-		}
-	}
-	return a;
-}
-
 function expand(mountain, n) {
 	let result = cloneMountain(mountain);
 	if (mountain[0].at(-1).parentIndex == -1) {
 		result[0].pop();
 	} else {
-		if (mountain.length == 1 || mountain[1].at(-1).value == 0) {
-			let array = mountain[0].map(v => v.value);
-			return calcMountain(arrayToRow(TrSSexpand(array, n)));
-		}
-
-		let cutHeight = mountain.findLastIndex((v, i) => v.at(-1).position + i == mountain[0].length - 1);
+		let cutHeight = mountain.findLastIndex(row => row.at(-1).position == mountain[0].length - 1);
 		for (let i = 0; i <= cutHeight; i++) result[i].pop();
 		if (!result.at(-1).length) result.pop();
 		let cutLength = result[0].length;
 
-		cutHeight--;
-		let badRootSeam = getBadRoot(mountain);
-		let badRootHeight = cutHeight;
+		let badRootHeight = cutHeight - 1;
+		let badRootRow = mountain[badRootHeight];
+		let badRootSeam = badRootRow[badRootRow.at(-1).parentIndex].position;
+
+		if (mountain[badRootHeight+1].at(-1).value == 0 && badRootRow.at(-1).value != 1) { // expand like Triangular Sequence System
+			let ancestry = [badRootRow.length-1];
+			while (badRootRow[ancestry.at(-1)].parentIndex != -1) {
+				ancestry.push(badRootRow[ancestry.at(-1)].parentIndex);
+			}
+			let cutNode = badRootRow[ancestry[0]].value;
+			let root = ancestry[cutNode - badRootRow[ancestry[1]].value];
+			let increment = cutNode - badRootRow[root].value - 1;
+			let badPart = badRootRow.slice(root, -1);
+			for (let i = 1; i <= n; i++) {
+				result[badRootHeight].push(...badPart.map(v => {
+					return {
+						value: v.value + increment*i,
+						position: v.position + badPart.length*i,
+						parentIndex: v.parentIndex + badPart.length*i,
+					};
+				}));
+			}
+			for (let k = badRootHeight - 1; k >= 0; k--) {
+				let parent = mountain[k].at(-1).parentIndex;
+				for (let i = result[k].length; i < result[badRootHeight].length; i++) {
+					let numAncestors = 1;
+					let current = parent;
+					while (result[k][current].parentIndex != -1) {
+						current = result[k][current].parentIndex;
+						numAncestors++;
+					}
+					result[k].push({
+						value: result[k][parent].value + result[k+1][i-1].value + numAncestors,
+						position: result[k][i-1].position + 1,
+						parentIndex: parent
+					});
+					parent = i;
+				}
+			}
+			return result;
+		}
 
 		for (let i = 1; i <= n; i++) {
 			for (let j = badRootSeam; j < cutLength; j++) {
 				let isAscending = false;
-				let p = mountain[badRootHeight].findIndex(x => x.position + badRootHeight >= j);
-				if (mountain[badRootHeight][p].position + badRootHeight == j) {
+				let p = badRootRow.findIndex(x => x.position >= j);
+				if (badRootRow[p].position == j) {
 					while (true) {
-						if (!mountain[badRootHeight][p] || mountain[badRootHeight][p].position + badRootHeight < badRootSeam) {
+						if (!badRootRow[p] || badRootRow[p].position < badRootSeam) {
 							isAscending = false;
 							break;
 						}
-						if (mountain[badRootHeight][p].position + badRootHeight == badRootSeam) {
+						if (badRootRow[p].position == badRootSeam) {
 							isAscending = true;
 							break;
 						}
-						p = mountain[badRootHeight][p].parentIndex;
+						p = badRootRow[p].parentIndex;
 					}
 				}
 
-				let seamHeight = 1 + result.findLastIndex((v, i) => {
-					let x = v.find(x => x.position + i >= j);
-					return x && x.position + i == j;
+				let seamHeight = 1 + result.findLastIndex(v => {
+					let x = v.find(x => x.position >= j);
+					return x && x.position == j;
 				});
 				let isReplacingCut = j == badRootSeam;
-				let klimit = seamHeight;
-				if (isAscending) {
-					klimit = seamHeight + (cutHeight-badRootHeight)*i;
-				}
-				for (let k = 0; k < klimit; k++) {
+				for (let k = 0; k < seamHeight; k++) {
 					if (!result[k]) result.push([]);
-					let sy = k;
-					let sx = 0;
-					if (isAscending && k >= badRootHeight) {
-						if (k <= badRootHeight + (cutHeight-badRootHeight)*(i-isReplacingCut)) {
-							sy = badRootHeight;
-						} else if (isReplacingCut && k <= badRootHeight+(cutHeight-badRootHeight)*i) {
-							sy = k - (cutHeight-badRootHeight)*(i-1);
-						} else {
-							sy = k - (cutHeight-badRootHeight)*i;
-						}
-						sx = mountain[sy].findIndex(x => x.position + sy >= j);
-					} else {
-						if (isReplacingCut) {
-							sx = mountain[sy].length - 1;
-						} else {
-							sx = mountain[sy].findIndex(x => x.position + sy >= j);
-						}
+					let sx = mountain[k].length - 1;
+					if (!isReplacingCut || (isAscending && k >= badRootHeight)) {
+						sx = mountain[k].findIndex(x => x.position >= j);
 					}
-					let sourceParentIndex = mountain[sy][sx].parentIndex;
+					let sourceParentIndex = mountain[k][sx].parentIndex;
 					let parentShifts = i - isReplacingCut;
 					let parentPosition = -1;
-					if (mountain[sy][sourceParentIndex]) {
-						let pos = mountain[sy][sourceParentIndex].position + sy;
+					if (mountain[k][sourceParentIndex]) {
+						let pos = mountain[k][sourceParentIndex].position;
 						if (pos >= badRootSeam) {
-							parentPosition = pos + parentShifts*(cutLength-badRootSeam) - k;
+							parentPosition = pos + parentShifts*(cutLength-badRootSeam);
 						} else {
-							parentPosition = pos - k;
+							parentPosition = pos;
 						}
 					}
 					let parentIndex = result[k].findIndex(x => x.position == parentPosition);
 					result[k].push({
 						value: parentIndex == -1 ? 0 : NaN,
-						position: j + (cutLength-badRootSeam)*i - k,
+						position: j + (cutLength-badRootSeam)*i,
 						parentIndex: parentIndex
 					});
 				}
@@ -250,7 +222,7 @@ function expand(mountain, n) {
 		}
 		for (let j = 0; j < result[i].length; j++) {
 			if (!isNaN(result[i][j].value)) continue;
-			let k = result[i+1].findIndex(x => x.position >= result[i][j].position - 1);
+			let k = result[i+1].findIndex(x => x.position >= result[i][j].position);
 			let numAncestors = 0;
 			let current = j;
 			while (result[i][current].parentIndex != -1) {
