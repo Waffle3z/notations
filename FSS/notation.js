f83dcba08785ff042019e7945197dc6f4afc6e74
Waffle <w4ffle3z@gmail.com>
2024-03-24 22:13:55 -0400

    FSS

---
+++ FSS/notation.js
@@ -0,0 +1,244 @@
+class notation {
+	static title = "FSS";
+	static header = "Fundamental Sequence System";
+
+	static lessOrEqual(a, b) {
+		return notation.toString(a).replaceAll(/./g, c => c == "[" ? 1 : 0) <= notation.toString(b).replaceAll(/./g, c => c == "[" ? 1 : 0);
+	}
+
+	static lessThan(a, b) {
+		return notation.toString(a).replaceAll(/./g, c => c == "[" ? 1 : 0) < notation.toString(b).replaceAll(/./g, c => c == "[" ? 1 : 0);
+	}
+
+	static equal(a, b) {
+		return notation.toString(a).replaceAll(/./g, c => c == "[" ? 1 : 0) == notation.toString(b).replaceAll(/./g, c => c == "[" ? 1 : 0);
+	}
+
+	static expandLimit(n) {
+		return limit(n+1);
+	}
+
+	static expand(a, n) {
+		return expand(a, n);
+	}
+
+	static isSuccessor(array) {
+		let s = notation.toString(array);
+		return s.length == 0 || s.endsWith("[]");
+	}
+
+	static toString(array) {
+		return JSON.stringify(array).slice(1,-1).replaceAll(/,/g, "");
+	}
+
+	static fromString(s) {
+		return JSON.parse("["+s.replaceAll(/\]\[/g,"],[")+"]");
+	}
+
+	static convertToNotation(value) {
+		if (value === "") return "∅";
+		let s = value.replaceAll(/\]\[/g,"],[").replaceAll(/\[\]/g,"0");
+		while (true) {
+			let next = s.replaceAll(/\[([0,]+)\]/g,(_, n) => `${(n.length+1)/2}`);
+			if (next == s) break;
+			s = next;
+		}
+		s = s.replaceAll(/\[[0-9,]+\]/g, function(x) {
+			let a = JSON.parse(x);
+
+			for (let i = a.length-2; i > 0; i--) { // standardize
+				if (a[i] == a[i-1] && a[i+1] > a[i]) {
+					a.splice(i, 1);
+				}
+			}
+			
+			for (let i = 1; i < a.length; i++) {
+				if (a[i] - a[i-1] > 1) return JSON.stringify(a);
+			}
+			return PrSStoCNF(standardizePrSS(a));
+		});
+		s = s.replaceAll("[0,1,3]", "ε₀");
+		s = s.replaceAll("[0,1,4]", "ζ₀");
+		s = s.replaceAll("[0,1,5]", "η₀");
+		s = s.replaceAll("[0,1,ω]", "φ(ω,0)");
+		return s;
+	}
+};
+
+function toString(a) {
+	return notation.convertToNotation(notation.toString(a));
+}
+
+// remove the rightmost []
+function decrement(a) {
+	if (a.length === 0) return [];
+	if (a[a.length - 1].length === 0) return a.slice(0, -1);
+	return a.slice(0, -1).concat([decrement(a[a.length - 1])]);
+}
+
+function searchForParent(root, target) {
+	// find limits where x[0] <= root < target < x
+	let candidates = [];
+	let current = [[[]]];
+	while (true) {
+		let rootIndex = findPrefixInExpansion(root, current)[1];
+		let [next, nextIndex] = findNextInExpansion(target, current);
+		if (nextIndex > rootIndex + 1 && rootIndex != -1) {
+			candidates.push(current);
+		}
+		current = next;
+		if (notation.isSuccessor(current)) break;
+	}
+
+	// find the candidate whose expansion contains the largest prefix of the root
+	let parent, prefix;
+	for (let i = 0; i < candidates.length; i++) {
+		let index = findNextInExpansion(root, candidates[i])[1];
+		let previous = expand(candidates[i], index - 1);
+		if (prefix == null || notation.lessThan(prefix, previous)) {
+			parent = candidates[i];
+			prefix = previous;
+		}
+	}
+
+	return parent;
+}
+
+// find the next greater term in the expansion of parent
+function findNextInExpansion(term, parent) {
+	if (notation.lessOrEqual(parent, term)) return [null, -1]; // parent should be greater than term
+	let index = 0;
+	let element = expand(parent, 0);
+	while (notation.lessOrEqual(element, term)) {
+		element = expand(parent, ++index);
+	}
+	return [element, index];
+}
+
+function findPrefixInExpansion(term, parent) {
+	if (notation.lessThan(parent, term)) return [null, -1]; // parent should be greater than or equal to term
+	let index = 0;
+	let element = expand(parent, 0);
+	if (notation.lessThan(term, element)) return [null, -1]; // term should be at least parent[0]
+	while (notation.lessOrEqual(element, term)) {
+		let next = expand(parent, index + 1);
+		if (!notation.lessOrEqual(next, term)) break;
+		element = next;
+		index++;
+	}
+	return [element, index];
+}
+
+function expand(a, n) {
+	let str = toString(a);
+	if (str === "1") return limit(n);
+	if (str === "0,1") return Array(n).fill([]);
+	if (a.length === 0) return [];
+	if (n == 0) {
+		for (let i = a.length - 2; i >= 0; i--) { // cut the last increasing sequence
+			if (notation.lessOrEqual(a[a.length - 1], a[i])) {
+				return a.slice(0, i + 1);
+			}
+		}
+		return a.slice(0, -1); // cut the last element if the whole sequence is increasing
+	}
+	a = [...a];
+	let cutNode = a.pop();
+	if (!notation.isSuccessor(cutNode)) {
+		a.push(expand(cutNode, n));
+		return a;
+	}
+
+	let predecessor = decrement(cutNode);
+	let rootIndex = a.findLastIndex(v => notation.lessThan(v, cutNode));
+	let root = a[rootIndex];
+	let badPart = [predecessor, ...a.slice(rootIndex + 1)];
+	if (notation.equal(root, predecessor)) { // cut node == root + 1
+		for (let i = 1; i <= n; i++) {
+			a.push(...badPart);
+		}
+		if (notation.isSuccessor(a)) {
+			a.pop();
+		}
+		return a;
+	}
+
+	let parent = searchForParent(root, predecessor);
+	let indexRoot = findPrefixInExpansion(root, parent)[1];
+	let indexPredecessor = findNextInExpansion(predecessor, parent)[1] - 1;
+	let increment = indexPredecessor - indexRoot;
+	for (let i = 1; i <= n; i++) {
+		// for each element in the bad part, if it's in the parent then move the index and copy the suffix
+		let copy = [...badPart].map((x, j) => {
+			if (notation.lessThan(x, parent)) {
+				let [prefix, index] = findPrefixInExpansion(x, parent);
+				let term = expand(parent, index + increment * (j == 0 ? i - 1 : i));
+				term.push(...x.slice(prefix.length)); // copy suffix
+				return term;
+			}
+			return x;
+		});
+		a.push(...copy);
+	}
+
+	return a;
+}
+
+function limit(n) {
+	if (n === 0) return [];
+	if (n === 1) return [[]];
+	return [[], limit(n-1)];
+}
+
+function standardizePrSS(s) {
+	let siblings = [];
+	let current;
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] == 0) {
+			current = [];
+			siblings.push(current);
+		}
+		current.push(s[i]);
+	}
+	for (let i = 0; i < siblings.length; i++) {
+		if (siblings[i].includes(1)) {
+			siblings[i] = [0, ...standardizePrSS(siblings[i].slice(1).map(x => x-1)).map(x => x+1)];
+		}
+	}
+	for (let i = siblings.length - 2; i >= 0; i--) {
+		if (siblings[i] < siblings[i+1]) {
+			siblings.splice(i, 1);
+		}
+	}
+	return siblings.flat();
+}
+
+function PrSStoCNF(s) {
+	let out = "";
+	let lastterm = "";
+	let coefficient = 1;
+	let root = 0;
+
+	for (let i = 0; i <= s.length; i++) {
+		if ((s[i + 1] === s[0]) || (i + 1 >= s.length)) {
+			let branches = 0;
+			for (let j = root+1; j <= i; j++) {
+				branches += s[j] === s[root+1] ? 1 : 0;
+			}
+
+			let term = ["1", "ω"][i - root] || (branches === 1 ? "ω^x" : "ω^(x)").replace("x", PrSStoCNF(s.slice(root+1, i+1))).replace(/\((\d+)\)/g, "$1");
+			if (term === lastterm && i !== s.length) {
+				coefficient += 1;
+			} else {
+				if (lastterm) {
+					out += "+" + (coefficient === 1 ? lastterm : lastterm === "1" ? coefficient : lastterm + (lastterm === "ω" ? "" : "·") + coefficient);
+				}
+				lastterm = term;
+				coefficient = 1;
+			}
+			root = i + 1;
+		}
+	}
+
+	return out.substring(1);
+}
