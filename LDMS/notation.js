86f639c72a39f846a3d444e2cf68917e4c395c0e
waffle <w4ffle3z@gmail.com>
2025-11-24 23:46:40 -0500

    LDMS working implementation

--- LDMS/notation.js
+++ LDMS/notation.js
@@ -5,6 +5,57 @@ function arrayCompare(a, b) { // -1 if a < b, 0 if a == b, 1 if a > b
 	return a.length < b.length ? -1 : a.length == b.length ? 0 : 1
 }
 
+function toPointerMatrix(a) {
+	let pointerMatrix = [];
+	for (let i = 0; i < a.length; i++) {
+		pointerMatrix[i] = [];
+		for (let j = 0; j < a[i].length; j++) {
+			pointerMatrix[i][j] = {distance: 0, delta: 0}
+		}
+		let parentIndex = a.findLastIndex((v, j) => j < i && v[0] < a[i][0]);
+		if (parentIndex != -1) {
+			pointerMatrix[i][0] = {distance: i - parentIndex, delta: a[i][0] - a[parentIndex][0]}
+		}
+	}
+	for (let i = 0; i < a.length; i++) {
+		if (pointerMatrix[i][0].delta == 0) continue;
+		for (let j = 1; j < a[i].length; j++) {
+			let parentIndex = i;
+			while (true) {
+				if ((a[parentIndex][j] || 0) < a[i][j]) {
+					break;
+				} else {
+					parentIndex -= pointerMatrix[parentIndex][j-1].distance;
+				}
+			}
+			pointerMatrix[i][j].distance = i - parentIndex;
+			pointerMatrix[i][j].delta = a[i][j] - (a[parentIndex][j] || 0);
+		}
+	}
+	return pointerMatrix;
+}
+
+function toMatrix(a) {
+	let matrix = [];
+	for (let i = 0; i < a.length; i++) {
+		matrix[i] = [];
+		for (let j = 0; j < a[i].length; j++) {
+			matrix[i][j] = a.delta == 0 ? 0 : (matrix[i-a[i][j].distance][j] || 0) + a[i][j].delta;
+		}
+	}
+	return matrix;
+}
+
+function deepcopy(pm) {
+	return pm.map(v => {
+		let a = v.map(x => {
+			return {distance: x.distance, delta: x.delta};
+		});
+		if (v.ascending) a.ascending = true;
+		return a;
+	});
+}
+
 class notation {
 	static title = "LDMS";
 	static header = "Large Descending Matrix System";
@@ -19,43 +70,60 @@ class notation {
 	}
 
 	static expandLimit(n) {
-		let s = "()(" + (n+1) + ")";
-		return this.fromString(s);
+		return [[0], [n + 1]];
 	}
 
 	static expand(matrix, n) {
-		const lastEntry = matrix.at(-1);
-		if (!lastEntry) return [];
-		const newMatrix = matrix.map(row => [...row]);
-		const cutNode = newMatrix.pop();
-		if (n == 0 || cutNode.length == 0) {
-			return newMatrix;
+		let pm = toPointerMatrix(matrix);
+		let rootIndex = pm.length - 1 - pm.at(-1).at(-1).distance;
+		if (n == 0) return toMatrix(pm.slice(0, rootIndex - 1));
+		let out = deepcopy(pm);
+		let tail = out.slice(rootIndex + 1);
+		
+		let cutNode = tail.at(-1);
+		let lastColumnIndex = cutNode.length - 1;
+		let lastColumn = cutNode[lastColumnIndex];
+		let lastColumnDistance = lastColumn.distance;
+		let ascend = (lastColumnIndex >= out[rootIndex].length || out[rootIndex][lastColumnIndex].distance == 0) ? lastColumn.delta - 1 : 0;
+		if (ascend == 0) {
+			cutNode.pop();
+			let root = out[rootIndex];
+			for (let i = lastColumnIndex; i < root.length; i++) {
+				cutNode[i] = {distance: root[i].distance + lastColumnDistance, delta: root[i].delta};
+			}
+		} else {
+			lastColumn.delta--;
+			out[rootIndex].ascending = true;
+			cutNode.ascending = true;
+			// descendants of an ascended term that have an extra column also ascend
+			for (let i = rootIndex; i < out.length; i++) {
+				let parentIndex = i - out[i].at(-1).distance;
+				let isLonger = out[parentIndex].length < out[i].length || out[parentIndex][out[i].length-1].distance == 0;
+
+				if (isLonger && out[parentIndex].ascending) out[i].ascending = true;
+			}
 		}
-		const rootIndex = newMatrix.findLastIndex((row, i) => {
-			if (arrayCompare(cutNode, row) <= 0) return false;
-			if (newMatrix.find((row2, j) => j > i && arrayCompare(row, row2) == 1)) return false;
-			return cutNode.at(-1) > (row[cutNode.length-1] || 0);
-		});
-		let rootNode = newMatrix[rootIndex];
-		const badPart = newMatrix.slice(rootIndex);
-		const delta = cutNode.at(-1) - (rootNode.at(cutNode.length-1) || 0);
+		let parentColumn = pm[rootIndex][lastColumnIndex];
+		if (parentColumn && lastColumn.delta == parentColumn.delta) lastColumn.distance = parentColumn.distance;
+		if (cutNode.length > 1 && lastColumn.delta == 0) cutNode.pop();
+		tail = deepcopy(tail);
+		cutNode = tail.at(-1);
 		for (let i = 1; i < n; i++) {
-			newMatrix.push(...badPart.map(row => {
-				const newRow = row.map((v, j) => {
-					let increment = Math.max(0, (cutNode[j] || 0) - (rootNode[j] || 0) - (j == cutNode.length-1 ? 1 : 0));
-					if (delta > 1 && cutNode.length < row.length) increment = delta - 1;
-					return v + increment * i;
-				});
-				const offset = cutNode.at(-1) - (row.at(cutNode.length-1) || 0);
-				if (offset > 1 && arrayCompare(row, cutNode) == -1) {
-					for (let b = 0; b < i; b++) {
-						newRow.push((offset - 1) * (i - b));
+			let copy = deepcopy(tail);
+			for (let j = 0; j < copy.length; j++) {
+				if (copy[j].ascending) {
+					for (let k = 0; k < i; k++) {
+						copy[j].unshift({distance: copy[j].at(-1).distance, delta: 1});
 					}
 				}
-				return newRow;
-			}));
+				for (let v of copy[j]) {
+					if (v.distance > j+1) v.distance += lastColumnDistance * i;
+				}
+			}
+			out.push(...copy);
 		}
-		return newMatrix;
+		out.pop(); // remove last copy of the cut node in case it's a successor
+		return toMatrix(out);
 	}
 
 	static isSuccessor(matrix) {
