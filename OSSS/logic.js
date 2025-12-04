23fb0486e28e3ab5982fedfff396ae43d9993d7d
waffle <w4ffle3z@gmail.com>
2025-12-04 18:29:30 -0500

    OSSS root finding algorithm

--- OSSS/logic.js
+++ OSSS/logic.js
@@ -8,7 +8,7 @@ function arrayToString(m) {
 
 function blockListToMatrix(a) {
 	return a.map((v, i) => {
-		let length = Math.max(0, ...a[i]);
+		const length = Math.max(0, ...v);
 		return [...Array(length + 1)].map((_, n) => v.includes(n) ? null : length - n).filter(n => n != null);
 	});
 }
@@ -16,7 +16,7 @@ function blockListToMatrix(a) {
 function matrixToBlockList(m, shortRows) {
 	return m.map((v, i) => {
 		if (v.length == 0 && i == 0) return [0];
-		let length = shortRows ? Math.max(0, ...v) + 1 : i;
+		const length = shortRows ? Math.max(0, ...v) + 1 : i;
 		return [...Array(length + 1)].map((_, n) => v.includes(length - n) ? null : n).filter(n => n != null);
 	});
 }
@@ -30,10 +30,10 @@ function shortNumber(n) {
 }
 
 function drawArray(a, showNumbers, shortRows) {
-	let rows = [];
-	let blockList = isBlockList(a) ? a : matrixToBlockList(a, shortRows);
+	const rows = [];
+	const blockList = isBlockList(a) ? a : matrixToBlockList(a, shortRows);
 	for (let i = 0; i < blockList.length; i++) {
-		let size = Math.max(0, ...blockList[i]) + 1;
+		const size = Math.max(0, ...blockList[i]) + 1;
 		rows.push(size == 0 ? "" : [...Array(size)].map((_, n) => {
 			if (blockList[i].includes(n)) return "█";
 			if (!showNumbers) return " ";
@@ -41,4 +41,50 @@ function drawArray(a, showNumbers, shortRows) {
 		}).join(""));
 	}
 	return rows.join("\n");
+}
+
+function getShadedRegion(blockList, shortRows) {
+	const matrix = blockListToMatrix(blockList);
+	const grid = blockList.map(row => {
+		const length = Math.max(0, ...row);
+		return [...Array(length + 1)].map((_, n) => row.includes(n) ? 0 : length - n);
+	});
+	const seen = grid.map(row => row.map(_ => false));
+	const y = grid.length - 1;
+	const x = grid[y].findLastIndex(x => x > 0);
+	if (x == -1) return [seen, []];
+	const v = grid[y][x];
+	const queue = [[y, x]];
+	const ancestorRows = [y];
+	while (true) {
+		const a = ancestorRows.at(-1);
+		const i = matrix.findLastIndex((v, i) => v[0] < matrix[a][0] && i < a);
+		if (i < 0) break;
+		ancestorRows.push(i);
+	}
+	const getPreviousRow = (r) => {
+		let i = ancestorRows.indexOf(r);
+		if (i == ancestorRows.length - 1) return null;
+		return ancestorRows[i + 1];
+	}
+	while (queue.length > 0) {
+		const [r, c] = queue.pop();
+		if (seen[r][c]) continue;
+		seen[r][c] = true;
+		const r2 = getPreviousRow(r);
+		if (r2) {
+			const dc = shortRows ? 0 : r2 - r + 1;
+			for (let c2 = c - 1 + dc; c2 <= c + 1 + dc; c2++) {
+				if (grid[r2][c2] >= v) queue.push([r2, c2]);
+			}
+		}
+		for (let c2 = c-1; c2 <= c+1; c2 += 2) {
+			if (grid[r][c2] >= v) queue.push([r, c2]);
+		}
+	}
+	const r = seen.findIndex(r => r.includes(true));
+	const c = seen[r].indexOf(true);
+	const ancestor = matrix.findLastIndex((v, i) => (!v[0] || v[0] < matrix[r][0]) && i < r);
+	const root = [ancestor, shortRows ? c - 1 : c - (r - ancestor)];
+	return [seen, root];
 }
