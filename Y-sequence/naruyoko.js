96896923286f0207789c5e1cb4a64f6e06f82124
waffle <w4ffle3z@gmail.com>
2026-02-20 23:36:32 -0500

    Y sequence simplify position offset

--- Y-sequence/naruyoko.js
+++ Y-sequence/naruyoko.js
@@ -9,11 +9,9 @@ function calcMountain(row) {
 				hasNextRow = true;
 				continue;
 			}
-			let p = 0;
-			if (mountain.length == 1) {
-				p = row[i].position + 1;
-			} else {
-				p = mountain.at(-2).findIndex(x => x.position > row[i].position);
+			let p = row[i].position + 1;
+			if (mountain.length > 1) {
+				p = mountain.at(-2).findIndex(x => x.position >= row[i].position);
 			}
 			while (p >= 0) {
 				let j = 0;
@@ -23,7 +21,7 @@ function calcMountain(row) {
 				} else {
 					p = mountain.at(-2)[p].parentIndex;
 					if (p < 0) break;
-					j = row.findIndex(x => x.position >= mountain.at(-2)[p].position - 1);
+					j = row.findIndex(x => x.position >= mountain.at(-2)[p].position);
 				}
 				if (j < 0 || (j < row.length-1 && row[j].position + 1 != row[j+1].position)) break;
 				if (row[j].value < row[i].value) {
@@ -40,7 +38,7 @@ function calcMountain(row) {
 			if (row[i].parentIndex != -1) {
 				newRow.push({
 					value: row[i].value - row[row[i].parentIndex].value,
-					position: row[i].position - 1,
+					position: row[i].position,
 					parentIndex: -1
 				});
 			}
@@ -55,29 +53,28 @@ function calcDiagonal(mountain) {
 	const diagonalTree = [];
 	for (let i = 0; i < mountain[0].length; i++) { // only one diagonal exists for each left-side-up diagonal line
 		for (let j = mountain.length-1; j >= 0; j--) { // prioritize the top
-			let k = mountain[j].findIndex(x => x.position + j >= i);
-			if (k == -1 || mountain[j][k].position + j != i) continue;
+			const k = mountain[j].findIndex(x => x.position >= i);
+			if (k == -1 || mountain[j][k].position != i) continue;
 			let height = j;
-			let lastIndex = k;
-			let last = mountain[height][lastIndex];
+			let last = mountain[j][k];
 			while (true) {
+				const row = mountain[height];
+				const parentRow = mountain[height-1];
 				if (height == 0) {
-					lastIndex = last.parentIndex;
+					last = row[last.parentIndex];
 				} else {
-					let l = mountain[height-1].findIndex(x => x.position == last.position + 1); // find right-down
-					l = mountain[height-1][l].parentIndex; // go to its parent=left-down
-					let m = mountain[height].findIndex(x => x.position >= mountain[height-1][l].position - 1); // find up-left of that=left
-					if (mountain[height][m].position == mountain[height-1][l].position - 1) { // left exists
-						lastIndex = m;
+					const l = parentRow[parentRow.find(x => x.position == last.position).parentIndex]; // find right-down, go to its parent=left-down
+					const m = row.find(x => x.position >= l.position); // find up-left of that=left
+					if (m.position == l.position) { // left exists
+						last = m;
 					} else {
 						height--;
-						lastIndex = l;
+						last = l;
 					}
 				}
-				last = mountain[height][lastIndex];
 				if (!last || last.parentIndex == -1) {
 					diagonal.push(mountain[j][k].value);
-					diagonalTree.push((last ? last.position : -1) + height);
+					diagonalTree.push(last ? last.position : height - 1);
 					break;
 				}
 			}
@@ -85,7 +82,7 @@ function calcDiagonal(mountain) {
 		}
 	}
 	return diagonal.map((v, i) => {
-		let pw = diagonal.findLastIndex((x, j) => j < i && x < v);
+		const pw = diagonal.findLastIndex((x, j) => j < i && x < v);
 		let p = diagonalTree[i];
 		while (p > 0 && diagonal[p] >= diagonal[i]) p = diagonalTree[p];
 		return {
@@ -104,8 +101,8 @@ function cloneMountain(mountain) {
 function getBadRoot(mountain) {
 	const diagonal = calcMountain(calcDiagonal(mountain));
 	if (diagonal[0].at(-1).value != 1) return getBadRoot(diagonal);
-	const i = mountain.findLastIndex((v, i) => v.at(-1).position + i == mountain[0].length-1);
-	return mountain[i-1][mountain[i-1].at(-1).parentIndex].position + i-1;
+	const i = mountain.findLastIndex((v, i) => v.at(-1).position == mountain[0].length-1);
+	return mountain[i-1][mountain[i-1].at(-1).parentIndex].position;
 }
 
 function mountainFromArray(a) {
@@ -125,7 +122,7 @@ function expand(mountain, n) {
 	if (mountain[0].at(-1).parentIndex == -1) {
 		result[0].pop();
 	} else {
-		let cutHeight = mountain.findLastIndex((v, i) => v.at(-1).position + i == mountain[0].length - 1);
+		let cutHeight = mountain.findLastIndex((v, i) => v.at(-1).position == mountain[0].length - 1);
 		for (let i = 0; i <= cutHeight; i++) result[i].pop(); // cut child
 		if (!result.at(-1).length) result.pop();
 		const cutLength = result[0].length;
@@ -144,8 +141,8 @@ function expand(mountain, n) {
 		} else {
 			newDiagonal = expand(calcMountain(diagonal), n)[0];
 			badRootHeight = mountain.findLastIndex((v, i) => {
-				const x = v.find(x => x.position + i >= badRootSeam);
-				return x && x.position + i == badRootSeam;
+				const x = v.find(x => x.position >= badRootSeam);
+				return x && x.position == badRootSeam;
 			});
 		}
 		const tailLength = cutHeight - badRootHeight;
@@ -154,17 +151,17 @@ function expand(mountain, n) {
 		for (let i = 1; i <= n; i++) { // iteration
 			for (let j = badRootSeam; j < cutLength; j++) { // seam
 				let isAscending = false;
-				let p = mountain[badRootHeight].find(x => x.position + badRootHeight >= j);
-				if (p.position + badRootHeight == j) {
-					while (p && p.position + badRootHeight > badRootSeam) {
+				let p = mountain[badRootHeight].find(x => x.position >= j);
+				if (p.position == j) {
+					while (p && p.position > badRootSeam) {
 						p = mountain[badRootHeight][p.parentIndex];
 					}
-					isAscending = p && p.position + badRootHeight == badRootSeam;
+					isAscending = p && p.position == badRootSeam;
 				}
 
 				const seamHeight = 1 + result.findLastIndex((v, i) => {
-					const x = v.find(x => x.position + i >= j);
-					return x && x.position + i == j;
+					const x = v.find(x => x.position >= j);
+					return x && x.position == j;
 				});
 				const isReplacingCut = j == badRootSeam;
 				const klimit = isAscending ? seamHeight + tailLength * i : seamHeight;
@@ -183,30 +180,30 @@ function expand(mountain, n) {
 						if (!yamakazi && isReplacingCut) {
 							sx = mountain[sy].length - 1;
 						} else {
-							sx = mountain[sy].findIndex(x => x.position + sy >= j);
+							sx = mountain[sy].findIndex(x => x.position >= j);
 						}
 					} else {
 						if (isReplacingCut) {
 							sx = mountain[sy].length - 1;
 						} else {
-							sx = mountain[sy].findIndex(x => x.position + sy >= j);
+							sx = mountain[sy].findIndex(x => x.position >= j);
 						}
 					}
 					const sourceParent = mountain[sy][mountain[sy][sx].parentIndex];
 					const parentShifts = i - isReplacingCut;
 					let parentPosition = -1;
 					if (sourceParent) {
-						const pos = sourceParent.position + sy;
+						const pos = sourceParent.position;
 						if (pos >= badRootSeam) {
-							parentPosition = pos + parentShifts*(cutLength-badRootSeam) - k;
+							parentPosition = pos + parentShifts*(cutLength-badRootSeam);
 						} else {
-							parentPosition = pos - k;
+							parentPosition = pos;
 						}
 					}
 					const parentIndex = result[k].findIndex(x => x.position == parentPosition);
 					result[k].push({
 						value: parentIndex == -1 ? newDiagonal[j+(cutLength-badRootSeam)*i].value : NaN,
-						position: j + (cutLength-badRootSeam)*i - k,
+						position: j + (cutLength-badRootSeam)*i,
 						parentIndex: parentIndex,
 					});
 				}
@@ -222,7 +219,7 @@ function expand(mountain, n) {
 		}
 		for (let j = 0; j < result[i].length; j++) {
 			if (!isNaN(result[i][j].value)) continue;
-			const k = result[i+1].findIndex(x => x.position >= result[i][j].position - 1); // find left-up
+			const k = result[i+1].findIndex(x => x.position >= result[i][j].position); // find left-up
 			result[i][j].value = result[i][result[i][j].parentIndex].value + result[i+1][k].value;
 		}
 	}
